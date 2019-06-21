377ceffcabcdd5576e9fad81ebbfbff8a335fb6a
takasago <vipqiv00@gmail.com>
2019-06-21 13:18:29 +0900

    FIX: add sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,11 @@ class SessionsController < ApplicationController
   def create
     auth = request.env['omniauth.auth']
     user = User.find_or_create_from_auth(auth)
+    # セッションの作成
+    session[:name] = auth[:info][:nickame]
+    session[:user_id] = auth[:uid]
+    session[:user_token] = auth[:credentials][:token]
+    session[:user_secret] = auth[:credentials][:secret]
     flash[:notice] = "ユーザ認証が完了しました。"
     redirect_to root_path
   end
