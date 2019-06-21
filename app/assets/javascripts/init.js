d630337d131c5953aff5dd5d373924bfa6340f76
takasago <vipqiv00@gmail.com>
2019-06-21 13:18:14 +0900

    UPDATE: dropdown to sidenav

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, {
-      "alignment": "center",
-      "coverTrigger": false,
-      "closeOnClick": true,
-    //   "constrainWidth": true
+    // var elems = document.querySelectorAll('.dropdown-trigger');
+    // var instances = M.Dropdown.init(elems, {
+    //   "alignment": "right",
+    //   "coverTrigger": false,
+    //   "closeOnClick": true,
+    // //   "constrainWidth": true
+    // });
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {
+        edge: 'right',
     });
     var elems = document.querySelectorAll('.tabs');
     var instances = M.Tabs.init(elems, {});
