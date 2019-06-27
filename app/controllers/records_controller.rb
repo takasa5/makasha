a2f8471f58918765ac9d4abe2602c25d687026b3
takasago <vipqiv00@gmail.com>
2019-06-27 18:17:15 +0900

    ADD: next button on records

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,4 +2,10 @@ class RecordsController < ApplicationController
   def index
     @posts = Post.limit(10).order("created_at desc")
   end
+
+  def next
+    @start = params[:start].to_i
+    @posts = Post.order("created_at desc").offset(@start).limit(10)
+    @terminal_flag= not(@posts.count == 10) # ちょうど10で終わる時対応遅れる．．．
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,13 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/signout', to: 'sessions#destroy'
   get '/setting' => 'pages#setting'
-  get 'users/:twitterid' => 'users#home'
-  get 'record/move' => 'pages#move'
-  get 'record/edit' => 'pages#edit'
-  patch 'record/edit/:id' => 'posts#update'
+  get '/users/:twitterid' => 'users#home'
+  get '/record/move' => 'pages#move'
+  get '/record/edit' => 'pages#edit'
+  patch '/record/edit/:id' => 'posts#update'
   get '/lp' => 'landing#index'
   get '/records' => 'records#index'
+  get '/records/next' => 'records#next'
 
   resources :users
   resources :posts
