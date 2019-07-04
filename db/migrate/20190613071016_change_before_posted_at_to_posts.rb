0cb6cd9634846e6461bba35aa99fe959c520ce7a
takasago <vipqiv00@gmail.com>
2019-07-04 17:31:21 +0900

    FIX: migration

--- db/migrate/20190613071016_change_before_posted_at_to_posts.rb
+++ db/migrate/20190613071016_change_before_posted_at_to_posts.rb
@@ -1,6 +1,8 @@
 class ChangeBeforePostedAtToPosts < ActiveRecord::Migration[5.2]
   def change
-    rename_column :posts, :before_posted_at, :period_before
-    change_column :posts, :period_before, 'integer USING CAST(period_before AS integer)'
+    remove_column :posts, :before_posted_at
+    add_column :posts, :period_before, :integer
+    # rename_column :posts, :before_posted_at, :period_before
+    # change_column :posts, :period_before, 'integer USING CAST(period_before AS integer)'
   end
 end
