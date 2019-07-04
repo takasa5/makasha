47e07d0e6e04a19bc8144bf95f2f6c482a7da9e6
takasago <vipqiv00@gmail.com>
2019-07-04 15:02:35 +0900

    ADD: beta deploy

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get pages_home_url
-    assert_response :success
+    assert_redirected_to lp_url
   end
 
 end
