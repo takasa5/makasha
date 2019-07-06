82fdccee2ef4e34fb7c95db653f6fd466f8cd366
takasago <vipqiv00@gmail.com>
2019-07-07 00:22:37 +0900

    FIX; get next record (close #3)

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,11 +1,12 @@
 class RecordsController < ApplicationController
   def index
+    @all_count = Post.all.count
     @posts = Post.limit(10).order("created_at desc")
   end
 
   def next
     @start = params[:start].to_i
     @posts = Post.order("created_at desc").offset(@start).limit(10)
-    @terminal_flag= not(@posts.count == 10) # ちょうど10で終わる時対応遅れる．．．
+    @all_count = Post.all.count
   end
 end
