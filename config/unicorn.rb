e494e4c0848a32c8dee5a11172b18f8727afa92d
takasago <vipqiv00@gmail.com>
2019-08-08 21:29:22 +0900

    ADD: bundle_gemfile

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,10 @@ rails_root = File.expand_path('../../', __FILE__)
 worker_processes 2
 working_directory rails_root
 
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "#{rails_root}/Gemfile"
+end
+
 listen "#{rails_root}/tmp/unicorn.sock" 
 pid "#{rails_root}/tmp/unicorn.pid" 
 
